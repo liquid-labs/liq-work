c6f6c9e5c56c3afb1d2f3aa4455fd6cef678b5a2
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-14 10:44:32 -0600

    fixed designation of work DB path

--- src/setup.mjs
+++ src/setup.mjs
@@ -1,7 +1,7 @@
 import * as fsPath from 'node:path'
 
 const setup = ({ app, model, reporter }) => {
-  app.liq.constants.WORK_DB_PATH = app.liq.home() + ''
+  app.liq.constants.WORK_DB_PATH = fsPath.join(app.liq.home(), 'work', 'work-db.yaml')
 }
 
 export { setup }
