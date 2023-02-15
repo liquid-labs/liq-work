3186ca9e8b233ee2e8988a70a693309719e71b54
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-15 14:17:53 -0600

    added simple test for WorkDB

---
+++ src/handlers/work/_lib/test/work-db.test.js
@@ -0,0 +1,13 @@
+/* global describe expect test */
+import * as fsPath from 'node:path'
+
+import { WorkDB } from '../work-db'
+
+describe('WorkDB', () => {
+  test('Reads database on initialization from liq constants', () => {
+    const app = { liq: { constants: { WORK_DB_PATH: fsPath.join(__dirname, 'data', 'work-db-a', 'work-db.yaml') }}}
+    const workDB = new WorkDB({ app })
+
+    expect(workDB.getData('work-liquid-labs/liq-work/1')).toBeTruthy()
+  })
+})
