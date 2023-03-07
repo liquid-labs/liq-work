4fdd656feb242f4d8f73f6a72dc76ee3d283debd
Zane Rockenbaugh <zane@liquid-labs.com>
2023-03-07 14:02:00 -0600

    fixed missing index entry for 'work issues remove'

--- src/handlers/work/issues/index.js
+++ src/handlers/work/issues/index.js
@@ -3,7 +3,8 @@ import * as addImpliedHandler from './add-implied'
 import * as listHandler from './list'
 import * as listImpliedHandler from './list-implied'
 import * as removeHandler from './remove'
+import * as removeImpliedHandler from './remove-implied'
 
-const handlers = [addHandler, addImpliedHandler, listHandler, listImpliedHandler, removeHandler]
+const handlers = [addHandler, addImpliedHandler, listHandler, listImpliedHandler, removeHandler, removeImpliedHandler]
 
 export { handlers }
