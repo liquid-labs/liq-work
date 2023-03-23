e18c618d9097a0db5e18873e2b0c65bb28c78e6d
Zane Rockenbaugh <zane@liquid-labs.com>
2023-03-23 06:16:12 -0500

    implement 'work build'

--- src/handlers/work/index.js
+++ src/handlers/work/index.js
@@ -1,6 +1,8 @@
 import { handlers as issueHandlers } from './issues'
 import { handlers as projectHandlers } from './projects'
 
+import * as buildHandler from './build'
+import * as buildImpliedHandler from './build-implied'
 import * as cleanHandler from './clean'
 import * as cleanImpliedHandler from './clean-implied'
 import * as pauseHandler from './pause'
@@ -15,6 +17,8 @@ import * as submitHandler from './submit'
 import * as submitImpliedHandler from './submit-implied'
 
 const handlers = [
+  buildHandler,
+  buildImpliedHandler,
   cleanHandler,
   cleanImpliedHandler,
   pauseHandler,
